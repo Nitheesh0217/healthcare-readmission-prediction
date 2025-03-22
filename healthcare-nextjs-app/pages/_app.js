8adcb9de3c9149f2e61db0efc732824fedf4c70a
Nitheesh Donepudi <166969412+Nitheesh0217@users.noreply.github.com>
2025-03-22 10:14:09 -0400

    Update _app.js

--- healthcare-nextjs-app/pages/_app.js
+++ healthcare-nextjs-app/pages/_app.js
@@ -1,10 +1,22 @@
-import '../styles/globals.css';
+// /pages/_app.js
+import { motion, AnimatePresence } from 'framer-motion';
 import Layout from '../components/Layout';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
